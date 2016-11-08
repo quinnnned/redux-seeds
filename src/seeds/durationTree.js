f930716ba08255f03dcd41ccd9f6750fc4cf5d30
quinnnned <quinnnned@gmail.com>
2016-11-07 19:08:39 -0600

    Finish durationTree and tests

--- src/seeds/durationTree.js
+++ src/seeds/durationTree.js
@@ -1,10 +1,18 @@
+import toggleTree from './toggleTree';
 
-const isUpperCamelCase = (text) => false;
+const isUpperCamelCase = (text) => {
+    if (text != `${text}`) return false;
+    return /^([A-Z][a-z]+)+$/.test(text);
+};
 
 export default (EventDescription) => {
     if ( !isUpperCamelCase(EventDescription) ) throw new TypeError(
         "The input to durationTree must be an UpperCamelCase string"
     );
-    
-    
+    return toggleTree({
+        defaultState : false,
+        selectorName : `is${EventDescription}`,
+        onActorName  : `start${EventDescription}`,
+        offActorName : `stop${EventDescription}`,
+    })
 };
--- test/durationTree.test.js
+++ test/durationTree.test.js
@@ -6,90 +6,55 @@ test('durationTree type', ({equal, end}) => {
     end();
 });
 
-// test('durationTree parameter validation', ({throws, doesNotThrow, end}) => {
-//     throws( () => durationTree('lowerCamelCase'), undefined, 'throws if input is not a UpperCamelCase string');
-//     doesNotThrow( () => durationTree('lowerCamelCase'), undefined, 'throws if input is not a UpperCamelCase string');
-//     end();
-// });
+test('durationTree parameter validation', ({throws, doesNotThrow, end}) => {
+    throws( () => durationTree(), /UpperCamelCase/, 'throws if input is omitted');
+    throws( () => durationTree(7),  /UpperCamelCase/, 'throws if input is not a string');
+    throws( () => durationTree('lowerCamelCase'),  /UpperCamelCase/, 'throws if input is not an UpperCamelCase string');
+    doesNotThrow( () => durationTree('UpperCamelCase'), 'does not throws if input is an UpperCamelCase string');
+    end();
+});
 
-// test('toggleTree output: state tree structure', ({deepEqual, equal, end}) => {
+test('durationTree output: state tree structure', ({equal, end}) => {
 
-//     const example = toggleTree({
-//         selectorName : 'exampleSelectorName',
-//         onActorName  : 'exampleOnActorName',
-//         offActorName : 'exampleOffActorName'
-//     });
+    const example = durationTree('DoingSomething');
 
+    equal(example == null, false, 'should not return null or undefined');
 
-//     equal(example == null, false, 'should not return null or undefined');
+    // .get
+    equal(typeof example.get.isDoingSomething, 'function', 'should have a selector named "is"+[EventDescription]')
+    equal(typeof example.get.isDoingSomething(), 'function', 'selector should be a function factory');
 
-//     // get
-//     equal(example.get == null, false, 'should have a "get" property');
-//     deepEqual( Object.keys(example.get), ['compose', 'exampleSelectorName'], 'get should have (2) properties called compose" and the user-provided selectorName' )
-//     equal(typeof example.get.exampleSelectorName, 'function', 'selector should be a function');
-//     equal(typeof example.get.exampleSelectorName(), 'function', 'selector should be a function factory');
+    // .act
+    equal(typeof example.act.startDoingSomething, 'function', 'should have an actor named "start"+[EventDescription]');
+    equal(typeof example.act.stopDoingSomething, 'function', 'should have an actor named "stop"+[EventDescription]');
 
-//     // act
-//     equal(example.act == null, false, 'should have an "act" property');
-//     deepEqual( Object.keys(example.act), ['compose', 'exampleOnActorName', 'exampleOffActorName'], 'act should have (3) properties called "compose" and the user-provided onActorName and offActorName');
-//     equal(typeof example.act.exampleOnActorName, 'function', 'on actor should be a function');
-//     equal(typeof example.act.exampleOffActorName, 'function', 'off actor should be a function');
+    // .reducer
+    equal(typeof example.reducer, 'function', 'should have a function "reducer" property');
+    end();
+});
 
-//     equal(typeof example.reducer, 'function', 'should have a function "reducer" property');
-//     end();
-// });
+test('durationTree default state', ({equal, end}) => {
+    const example = durationTree('MakingFriends');
+    equal( example.reducer(), false, 'reducer should return false by default' );
+    equal( example.get.isMakingFriends()(), false, 'selector should return false by default');
+    end();
+});
 
-// test('toggleTree default state', ({equal, end}) => {
+test('durationTree actions', ({deepEqual, end}) => {
+    const example = durationTree('WritingCode');
+    deepEqual(example.act.startWritingCode(), { type: 'START_WRITING_CODE' }, "tree.act.start[EventDescription] should return an action with type equal to START_[EVENT_DESCRIPTION]");
+    deepEqual(example.act.stopWritingCode(), { type: 'STOP_WRITING_CODE' }, "tree.act.stop[EventDescription] should return an action with type equal to STOP_[EVENT_DESCRIPTION]");
+    end();
+});
 
-//     const example = toggleTree({
-//         selectorName : 'exampleSelectorName',
-//         onActorName  : 'exampleOnActorName',
-//         offActorName : 'exampleOffActorName'
-//     });
-
-//     equal( example.reducer(), false, 'reducer should return false by default' );
-//     equal( example.get.exampleSelectorName()(), false, 'selector should return false by default');
-
-//     const exampleDefaultTrue = toggleTree({
-//         defaultState : true,
-//         selectorName : 'exampleSelectorName',
-//         onActorName  : 'exampleOnActorName',
-//         offActorName : 'exampleOffActorName'
-//     });
-
-//     equal( exampleDefaultTrue.reducer(), true, 'if defaultState is true, reducer should return true by default' );
-//     equal( exampleDefaultTrue.get.exampleSelectorName()(), true, 'if defaultState is true, selector should return true by default');
-//     end();
-// });
-
-// test('toggleTree actions', ({deepEqual, end}) => {
-
-//     const example = toggleTree({
-//         selectorName : 'exampleSelectorName',
-//         onActorName  : 'turnTheThingOn',
-//         offActorName : 'turnTheThingOff'
-//     });
-
-//     deepEqual(example.act.turnTheThingOn(), { type: 'TURN_THE_THING_ON' }, "tree.act.[onActorName] should return an action with type equal to the UPPER_SNAKE_CASE equivalent of onActorName.");
-//     deepEqual(example.act.turnTheThingOff(), { type: 'TURN_THE_THING_OFF' }, "tree.act.[offActorName] should return an action with type equal to the UPPER_SNAKE_CASE equivalent of offActorName.");
-//     end();
-// });
-
-// test('toggleTree reducer', ({equal, deepEqual, end}) => {
-
-//     const example = toggleTree({
-//         selectorName : 'isTheThingOn',
-//         onActorName  : 'turnTheThingOn',
-//         offActorName : 'turnTheThingOff'
-//     });
-
-//     const test = (...actions) => example.get.isTheThingOn()( actions.reduce(example.reducer, {} ) );
-//     const on = example.act.turnTheThingOn();
-//     const off = example.act.turnTheThingOff();
-//     const etc = { type: "SOME_UNRELATED_ACTION_TYPE" }
-
-//     equal( test(on), true, 'selector should return true for state produced by an [onActorName] action');
-//     equal( test(on, off), false, 'selector should return false for state produced by an [offActorName] action');
-//     deepEqual( test(on), test(on, etc), 'state should not be affected by unrelated actions');
-//     end();
-// });
+test('durationTree reducer', ({equal, deepEqual, end}) => {
+    const example = durationTree('SlayingDragons')
+    const test = (...actions) => example.get.isSlayingDragons()( actions.reduce(example.reducer, {} ) );
+    const start = example.act.startSlayingDragons();
+    const stop = example.act.stopSlayingDragons();
+    const etc = { type: "SOME_UNRELATED_ACTION_TYPE" }
+    equal( test(start), true, 'selector should return true for state produced by a start[EventDescription] action');
+    equal( test(start, stop), false, 'selector should return false for state produced by a stop[EventDescription] action');
+    equal( test(start), test(start, etc), 'state should not be affected by unrelated actions');
+    end();
+});
